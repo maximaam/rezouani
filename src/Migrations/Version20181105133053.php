15ac9554469ffc62e431412d72ae8fa2f327a773
Maamar Khemissi <maamar.khemissi@meingutscheincode.com>
2018-11-06 10:10:48 +0100

    upds

---
+++ src/Migrations/Version20181105133053.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181105133053 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE category CHANGE description_de description_de LONGTEXT DEFAULT NULL, CHANGE description_en description_en LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE category CHANGE description_de description_de LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, CHANGE description_en description_en LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
